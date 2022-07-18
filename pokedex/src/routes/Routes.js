437284db06ea89891dd8d546c3b5af2d18cfad57
Mariana <mariandrade8115@gmail.com>
2022-07-18 13:56:38 -0300

    mari1 - rotas

---
+++ pokedex/src/routes/Routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Pages/HomePage/HomePage"
+import DetailPage from "../Pages/DetailPage/DetailPage"
+import Pokedex from "../Pages/Pokedex/Pokedex"
+import ErrorPage from "../Pages/ErrorPage/ErrorPage"
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+        <Route index
+        element = {<Home />} />
+        <Route path= "/detailPage/:id"
+        element = {<DetailPage />} />
+        <Route path="/pokedex"
+        element = {<Pokedex />} />
+        <Route path="/errorPage"
+        element = {<ErrorPage />} />
+        </Routes>
+        </BrowserRouter>
+    )
+} 
+
+
+
+
+
+
+
+
